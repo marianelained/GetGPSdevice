267d80135337e037b6b31019745e1f7850896068
Marian <medatup@gmail.com>
2023-05-23 23:34:05 +0800

    getGPS
    This python webapp allows you to get the current device GPS location and save it to csv, then print the data on the webpage.

---
+++ getGPSApp.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import geocoder
+import pandas as pd
+
+def main():
+    st.title("Device GPS Location")
+
+    # Get the current GPS location
+    location = geocoder.ip('me').latlng
+
+    # Print the location
+    st.write("Latitude:", location[0])
+    st.write("Longitude:", location[1])
+
+    # Create DataFrame
+    df = pd.DataFrame({'Latitude': [location[0]],
+                       'Longitude': [location[1]]
+                       })
+
+    # Save GPS data to CSV
+    save_gps_data(df)
+
+    # Display saved GPS data
+    display_saved_gps_data()
+
+def save_gps_data(df):
+    df.to_csv("gps.csv", mode='a', index=False, header=False)
+
+def display_saved_gps_data():
+    df = pd.read_csv("gps.csv")
+    st.dataframe(df)
+
+if __name__ == '__main__':
+    main()
+
+#this code obtains the device's GPS location and displays ot on the webapp in dataframe
